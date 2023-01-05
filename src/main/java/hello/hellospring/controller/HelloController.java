8831a765bd429e51ce8a961ffd95c9a41607e89e
ume <yoooomi030@gmail.com>
2023-01-05 21:14:03 +0900

    정적 컨텐츠, mvc와 템플릿 엔진에 대한 이해

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller //컨트롤러 어노테이션
 public class HelloController {
@@ -15,5 +16,12 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello"; //templates에 있는 hello.html을 찾아 렌더링
         //resources:templates/ + {viewName} + .html
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){
+        //required = true 기본으로 값을 넘겨야함.
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
